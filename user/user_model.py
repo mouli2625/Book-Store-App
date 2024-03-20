3b0616b02fd50848b080c2cc0e96a70714e3c681
Chandra Mouli <101442627+mouli2625@users.noreply.github.com>
2024-03-20 17:17:10 +0530

    [mouli]

--- user/user_model.py
+++ user/user_model.py
@@ -1,5 +1,7 @@
 from user import db
 from passlib.hash import pbkdf2_sha256
+from flask_jwt_extended import create_access_token
+from datetime import datetime,timedelta
 
 
 class User(db.Model):
@@ -10,10 +12,25 @@ class User(db.Model):
     email=db.Column(db.String(150),nullable=False,unique=False)
     is_superuser=db.Column(db.Boolean, default=False)
     is_verified=db.Column(db.Boolean, default=False)
-
-
-def verify_password(self, raw_password):
-    return pbkdf2_sha256.verify(raw_password, self.password)
+    
+    
+    def __init__(self,username,password,email,**kwargs):
+        self.username=username
+        self.password=pbkdf2_sha256.hash(password)
+        self.email=email
+        self.__dict__.update(kwargs)
+        
+        
+        
+    def verify_password(self, raw_password):
+        # print(pbkdf2_sha256.verify(raw_password, self.password))
+        return pbkdf2_sha256.verify(raw_password, self.password)
+        
+        
+    def token(self, aud='default', exp=15):
+        return create_access_token(identity=self.user_id,
+                                additional_claims={'exp':datetime.utcnow()+timedelta(minutes=exp),
+                                                    'aud': aud})
 
     
         
