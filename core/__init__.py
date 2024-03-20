d725aefc7093b2b3906fabeff49cf5a90690c090
Chandra Mouli <101442627+mouli2625@users.noreply.github.com>
2024-03-20 17:16:24 +0530

    [mouli]

--- core/__init__.py
+++ core/__init__.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from settings import settings
 
 
 
@@ -6,6 +7,12 @@ def init_app(database):
     app = Flask(__name__)
     app.config['SQLALCHEMY_DATABASE_URI']=f'postgresql+psycopg2://postgres:satyamouli@localhost:5432/{database}'
     app.config['SQL_ALCHEMY_TRACK_MODIFICATIONS']=True
+    app.config['JWT_SECRET_KEY'] = settings.jwt_key
+    app.config['MAIL_SERVER']='smtp.gmail.com'
+    app.config['MAIL_PORT']=465
+    app.config['MAIL_USE_SSL']= True
+    app.config['MAIL_USERNAME']=settings.email_sender
+    app.config['MAIL_PASSWORD']=settings.email_password
     return app
 
 
--- user/__init__.py
+++ user/__init__.py
@@ -1,11 +1,17 @@
 from core import init_app
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from flask_jwt_extended import JWTManager
+from flask_mail import Mail
 
 
 app=init_app("User")
 db= SQLAlchemy()
 migrate=Migrate()
+jwt=JWTManager()
+mail=Mail()
 db.init_app(app)
 migrate.init_app(app, db)
+jwt.init_app(app)
+mail.init_app(app)
 
