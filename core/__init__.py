d3f30f7153a0c054abef5183cd4b87144947abaa
Chandra Mouli <101442627+mouli2625@users.noreply.github.com>
2024-03-28 17:26:44 +0530

    [mouli] refactored code due to testing

--- core/__init__.py
+++ core/__init__.py
@@ -1,19 +1,34 @@
 from flask import Flask
 from settings import settings
+from flask_mail import Mail
+from flask_jwt_extended import JWTManager
 
+mail=Mail()
+jwt=JWTManager()
 
-
-def init_app(database):
+def init_app(database,mode="debug"):
     app = Flask(__name__)
-    app.config['SQLALCHEMY_DATABASE_URI']=f'postgresql+psycopg2://postgres:satyamouli@localhost:5432/{database}'
-    app.config['SQL_ALCHEMY_TRACK_MODIFICATIONS']=True
+    if mode == "debug":
+        app.config['SQLALCHEMY_DATABASE_URI']=f'postgresql+psycopg2://postgres:satyamouli@localhost:5432/{database}'
+        app.config['SQL_ALCHEMY_TRACK_MODIFICATIONS']=True
+        app.config['DEBUG']=True
+
+    if mode == "test":
+        app.config['SQLALCHEMY_DATABASE_URI']='sqlite:///:memory:'
+        app.config['SQL_ALCHEMY_TRACK_MODIFICATIONS']=True
+        app.config['TESTING']=True
+    
+    
     app.config['JWT_SECRET_KEY'] = settings.jwt_key
     app.config['MAIL_SERVER']='smtp.gmail.com'
     app.config['MAIL_PORT']=465
     app.config['MAIL_USE_SSL']= True
     app.config['MAIL_USERNAME']=settings.email_sender
     app.config['MAIL_PASSWORD']=settings.email_password
+    mail.init_app(app)
+    jwt.init_app(app)
     return app
+
 
 
 
