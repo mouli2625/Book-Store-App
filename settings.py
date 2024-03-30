b6a0e00065eb358628ca64d1373c5d0c15249f91
Chandra Mouli <101442627+mouli2625@users.noreply.github.com>
2024-03-30 18:31:04 +0530

    [mouli] added : code for logger

--- settings.py
+++ settings.py
@@ -1,4 +1,5 @@
 from pydantic_settings import BaseSettings,SettingsConfigDict
+from logging.config import dictConfig
 
 class Settings(BaseSettings):
     model_config = SettingsConfigDict(env_file='.env', extra='ignore')
@@ -8,5 +9,37 @@ class Settings(BaseSettings):
     jwt_algo: str
     email_sender: str
     email_password: str
+
+dictConfig(
+    {
+        "version": 1,
+        "formatters": {
+            "default": {
+                "format": "[%(asctime)s]: %(filename)s: %(levelname)s: %(lineno)d: %(message)s",
+                "datefmt": "%m/%d/%Y %I:%M:%S %p",
+                "style": "%",
+            },
+            "console": {"format": "%(message)s"},
+        },
+        "handlers": {
+            "console": {
+                "level": "INFO",
+                "class": "logging.StreamHandler",
+                "stream": "ext://sys.stdout",
+                "formatter": "console",
+            },
+            "file": {
+                "level": "WARNING",
+                "class": "logging.FileHandler",
+                "filename": "book_store.log",
+                "formatter": "default",
+            },
+        },
+        # "root": {"level": "INFO", "handlers": ["console"]},
+        "loggers": {
+            "": {"level": "DEBUG", "handlers": ["file", "console"], "propagate": True}
+        },
+    }
+)
 
 settings=Settings()
